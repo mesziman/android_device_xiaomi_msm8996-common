dff19ddd4efc428e1c898fa592609d614919444e
Alex Naidis <alex.naidis@linux.com>
2016-09-14 22:28:15 +0200

    common: Add QPerformance to boot jars
    Improves latency of perflocks.
    
    Change-Id: Iccb6e8395b60fd56a4f7c6b2213e5b56c04dab16
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>

--- common.mk
+++ common.mk
@@ -218,6 +218,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# QPerformance
+PRODUCT_BOOT_JARS += QPerformance
+
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
